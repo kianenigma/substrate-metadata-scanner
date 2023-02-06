b19566e6e1b3ad421238cebb0f149f3cf26c2121
kianenigma <kian@parity.io>
2023-02-06 15:14:52 -0300

    make it work

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,76 @@ import '@polkadot/api-augment';
 import '@polkadot/types-augment';
 
 import { ApiPromise, WsProvider } from '@polkadot/api';
-import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
-import { allChains } from './wss';
+import { allChains, kusamaParas, polkadotParas, rococoParas, westendParas } from './wss';
+import { Codec } from '@polkadot/types-codec/types';
+import { writeFileSync } from 'fs';
 
 async function main() {
-	for (const [key, value] of Object.entries(allChains)) {
-		console.log(
-			'\n----------------------------------- ',
-			key,
-			' -----------------------------------\n'
-		);
-		for (const chain of value) {
-			if (Object.keys(chain.providers).length) {
-				console.log('Name: ', chain.text, ' | providers: ', chain.providers);
+	const output: Map<string, [number, number]> = new Map();
+
+	// todo: concat these in a type-safe way. only take the chain name and provider, then concat.
+	// @ts-ignore
+	const paras = kusamaParas.concat(polkadotParas);
+
+	const tryProvider = async (s: string, chain: string): Promise<boolean> => {
+		const provider = new WsProvider(s);
+
+		// bueno: https://polkadot.js.org/docs/api/start/create#advanced-creation
+		const apiBuilder = new ApiPromise({ provider });
+
+		const api = await apiBuilder.isReadyOrError.catch((e) => {
+			console.error(e);
+		});
+
+		if (api && api.isConnected) {
+			const specName = api.runtimeVersion.specName.toString();
+			const instances = api.registry.getModuleInstances(specName, 'electionsPhragmen') || [];
+			const number = (x: Codec) => api.createType('u32', x).toNumber();
+			for (const instance of instances) {
+				if (
+					api.consts[instance] &&
+					api.consts[instance].maxVoters &&
+					api.consts[instance].maxCandidates
+				) {
+					console.log(api.consts[instance].maxVoters.toHuman());
+					output.set(chain, [
+						number(api.consts[instance].maxVoters),
+						number(api.consts[instance].maxCandidates)
+					]);
+				}
 			}
+			await api.disconnect();
+			return true;
+		} else {
+			await apiBuilder.disconnect();
+			return false;
 		}
-	}
+	};
+
+	let success = 0;
+	await Promise.all(
+		paras.map(async (chain) => {
+			if (Object.values(chain.providers).length) {
+				const providers = Object.values(chain.providers);
+				if (providers.length == 0) {
+					return;
+				}
+				for (let i = 0; i < providers.length; i++) {
+					if (await tryProvider(providers[i], chain.text)) {
+						console.log(`✅ connected to ${chain.text}`);
+						success += 1;
+						break;
+					} else {
+						console.log(`❌ failed to connected to ${chain.text} via ${providers[i]}, retrying.. `);
+					}
+				}
+			}
+		})
+	);
+
+	console.log(output);
+	console.log(paras.length, success, output.size);
+	writeFileSync('output.json', JSON.stringify(Object.fromEntries(output)));
 }
 
 main().catch(console.error);
